ce7967289f52bf337586ddfc5e78793b640e42bd
jacantwell <jasper66018@gmail.com>
2025-04-18 02:12:14 +0100

    Log -> blog

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -7,7 +7,7 @@ export function Intro() {
         Jasper. UK to ?
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A log of my travels around the world.
+        A blog of my travels around the world.
       </h4>
     </section>
   );
